254b694df127319e9645a0188ab3335124f308c3
Hasmire <lagman.jrgreggvictor@gmail.com>
2023-11-27 09:53:22 +0800

    Added theme

--- app/src/main/java/com/example/medtrack/MainActivity.kt
+++ app/src/main/java/com/example/medtrack/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.medtrack.ui.theme.MedTrackTheme
+import com.example.medtrack.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MedTrackTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
@@ -37,7 +37,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MedTrackTheme {
+    AppTheme {
         Greeting("Android")
     }
 }

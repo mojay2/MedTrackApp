88187710de5c8967dadc9e32dbb8c3881fcdb728
jay <86312510+mojay2@users.noreply.github.com>
2023-11-28 05:38:57 +0800

    Add basic navigation

--- app/src/main/java/com/example/medtrackbackend/MainActivity.kt
+++ app/src/main/java/com/example/medtrackbackend/MainActivity.kt
@@ -10,23 +10,64 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.medtrackbackend.ui.dashboard.DashboardScreen
+import com.example.medtrackbackend.ui.screens.add_edit_medicine.AddEditMedicineScreen
+import com.example.medtrackbackend.ui.screens.add_edit_program.AddEditProgramScreen
+import com.example.medtrackbackend.ui.screens.medicine_cabinet.MedicineCabinetScreen
+import com.example.medtrackbackend.ui.screens.medicine_details.MedicineDetailsScreen
 import com.example.medtrackbackend.ui.theme.MedTrackBackendTheme
+
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MedTrackBackendTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    DashboardScreen()
+//                }
+//            }
+//        }
+//    }
+//}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val navController = rememberNavController()
             MedTrackBackendTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    DashboardScreen()
+                NavHost(navController = navController, startDestination = "MedicineCabinet") {
+                    composable("MedicineCabinet") { MedicineCabinetScreen(navController) }
+                    composable("AddEditMedicine") { AddEditMedicineScreen(navController) }
+                    composable(
+                        "MedicineDetails/{medicineId}",
+                        arguments = listOf(navArgument("medicineId") { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        val medicineId = backStackEntry.arguments?.getString("medicineId") ?: ""
+                        MedicineDetailsScreen(navController, medicineId)
+                    }
+                    composable(
+                        "AddEditProgram/{medicineId}",
+                        arguments = listOf(navArgument("medicineId") { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        val medicineId = backStackEntry.arguments?.getString("medicineId") ?: ""
+                        AddEditProgramScreen(navController, medicineId)
+                    }
                 }
             }
         }
+
     }
 }
 
